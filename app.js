bbc57db89a48e4a21d7a75970675c3cd7f281afc
Martin Zane <realmartinzane@gmail.com>
2019-10-10 15:03:26 +0200

    implement reading data from inputs

--- app.js
+++ app.js
@@ -7,19 +7,44 @@ var budgetController = (function()
 // UI CONTROLLER
 var UIController = (function() 
 {
-    // Code
+
+    var DOMstrings = 
+    {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+    
+    return {
+        getInput: function() 
+        {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+
+        getDOMstrings: function()
+        {
+            return DOMstrings;
+        }
+    }
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) 
 {
+    var DOMstrings = UICtrl.getDOMstrings();
 
     var ctrlAddItem = function()
     {
-        console.log('Enter!')
+        var input = UICtrl.getInput();
+        console.log(input)
     }
     
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOMstrings.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) 
     {
