e04fd67357e73e2ee2d8f8591425b0c03588a43b
Martin Zane <realmartinzane@gmail.com>
2019-10-11 14:21:17 +0200

    display current date

--- app.js
+++ app.js
@@ -162,7 +162,8 @@ var UIController = (function()
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercentageLabel: '.item__percentage'
+        expensesPercentageLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     }
 
     var formatNumber = function(num, type) 
@@ -275,6 +276,17 @@ var UIController = (function()
                 else
                     cur.textContent = '---';
             });
+        },
+
+        dateDisplay: function()
+        {
+            var now, year, month, months;
+            now = new Date();
+            year = now.getFullYear();
+            month = now.getMonth();
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
         }
     }
 })();
@@ -365,6 +377,7 @@ var controller = (function(budgetCtrl, UICtrl)
     return {
         init: function()
         {
+            UICtrl.dateDisplay();
             UICtrl.budgetDisplay(
                 {
                     budget: 0,
